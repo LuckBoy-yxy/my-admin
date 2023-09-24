978bb1627c03f9a19887f009c36ce7bfd2765075
yuanxiaoran <hmxs_hmbb@163.com>
2023-09-24 21:22:36 +0800

    渲染用户头像结构、样式和逻辑已完成

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import user from './modules/user.js'
 
 const getters = {
   token: state => state.user.token,
+  userInfo: state => state.user.userInfo,
   hasUserInfo: state => JSON.stringify(state.user.userInfo) !== '{}'
 }
 
