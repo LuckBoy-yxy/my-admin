8ad5cfd2821bbd07923daa6464a1d5dc79757aa1
yuanxiaoran <hmxs_hmbb@163.com>
2023-09-27 19:13:00 +0800

    element-plus 新主体颜色未立即生效问题已解决

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <RouterView />
 </template>
 
-<script>
+<script setup>
+  import { useStore } from 'vuex'
+  import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 
+  const store = useStore()
+  generateNewStyle(store.getters.mainColor).then(newStyleText => {
+    writeNewStyle(newStyleText)
+  })
 </script>
 
 <style>
