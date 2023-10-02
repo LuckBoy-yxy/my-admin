1150d12740a74ff87c0cb1173a3e2cb63018610a
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-02 18:30:14 +0800

    个人中心结构已搭建完毕

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,20 +1,44 @@
 <script setup>
-  import {} from 'vue'
+  import { ref } from 'vue'
+  import ProjectCard from './components/ProjectCard.vue'
+  import Chapter from './components/Chapter.vue'
+  import Feature from './components/Feature.vue'
+  import Author from './components/Author.vue'
+
+  const activeName = ref('feature')
 </script>
 
 <template>
-  <div class="">
+  <div class="my-container">
     <el-row>
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
+      <el-col :span="6">
+        <ProjectCard class="project-card" />
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <Feature />
+            </el-tab-pane>
+
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <Chapter />
+            </el-tab-pane>
+
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <Author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.my-container {
+  .project-card {
+    margin-right: 20px;
+  }
+}
 </style>
