9c082ff8e379698b7f24bdb886ebf578e4620ae5
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-06 16:08:53 +0800

    项目介绍模块开发完毕

--- src/api/user.js
+++ src/api/user.js
@@ -4,3 +4,8 @@ import request from '@/utils/request'
 export const getUserInfo = () => {
   return request('/sys/profile', 'get')
 }
+
+// 获取个人中心, 项目功能数据
+export const feature = () => {
+  return request('/user/feature', 'get')
+}
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,18 +1,30 @@
 <script setup>
   import { ref } from 'vue'
+
+  import { feature } from '@/api/user'
   import ProjectCard from './components/ProjectCard.vue'
   import Chapter from './components/Chapter.vue'
   import Feature from './components/Feature.vue'
   import Author from './components/Author.vue'
 
   const activeName = ref('feature')
+  const featureData = ref([])
+  const getFeatureData = async () => {
+    const res = await feature()
+    featureData.value = res
+    console.log(res)
+  }
+  getFeatureData()
 </script>
 
 <template>
   <div class="my-container">
     <el-row>
       <el-col :span="6">
-        <project-card class="user-card"></project-card>
+        <project-card
+          class="project-card"
+          :features="featureData"
+        ></project-card>
       </el-col>
       <el-col :span="18">
         <el-card>
