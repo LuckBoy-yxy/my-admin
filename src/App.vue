d1b4b951539d25e9d2f5a89df456805435ce60ce
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-12 16:29:50 +0800

    升级element-plus版本, 组件国际化不自动更新的问题已解决

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <script setup>
+  import { computed } from 'vue'
   import { useStore } from 'vuex'
+  import zhCn from 'element-plus/es/locale/lang/zh-cn'
+  import en from 'element-plus/lib/locale/lang/en'
+
   import { generateNewStyle, writeNewStyle } from '@/utils/theme'
   import { watchSwitchLang } from '@/utils/i18n'
-
+  
   const store = useStore()
   generateNewStyle(store.getters.mainColor).then(newStyleText => {
     writeNewStyle(newStyleText)
@@ -16,6 +22,10 @@
     if (store.getters.token) {
       store.dispatch('user/getUserInfo')
     }
+  })
+
+  const locale = computed(() => {
+    return store.getters.language === 'en' ? en : zhCn
   })
 </script>
 
