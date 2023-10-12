18175356a5fe6c0fc75d1520f2449252991da8d2
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-12 17:31:25 +0800

    升级element-plus版本, 登录页 el-input 样式问题已解决

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -60,61 +60,58 @@
 </script>
 
 <template>
-  <div class="login">
-    <div class="login-form">
-      <div class="form-title">
-        <h3>{{ $t('msg.login.title') }}</h3>
-        <LangSelect class="lang-select" />
+  <div class="login-container">
+    <el-form
+      class="login-form"
+      ref="form"
+      :model="formData"
+      :rules="loginRules"
+    >
+      <div class="title-container">
+        <h3 class="title">{{ $t('msg.login.title') }}</h3>
+        <lang-select class="lang-select" effect="light"></lang-select>
       </div>
 
-      <el-form
-        ref="form"
-        :model="formData"
-        :rules="loginRules"
-        atuocomplete="off"
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon="user" />
+        </span>
+        <el-input
+          placeholder="username"
+          name="username"
+          type="text"
+          v-model="formData.username"
+        />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon="password" />
+        </span>
+        <el-input
+          placeholder="password"
+          name="password"
+          :type="passwordType"
+          v-model="formData.password"
+        />
+        <span class="show-pwd">
+          <svg-icon
+            :icon="isShowPwd ? 'eye-open' : 'eye'"
+            @click="isShowPwd = !isShowPwd"
+          />
+        </span>
+      </el-form-item>
+
+      <el-button
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        :loading="loading"
+        @click="handleLoginClick"
+        >{{ $t('msg.login.loginBtn') }}</el-button
       >
-        <el-form-item prop="username">
-          <span class="svg">
-            <SvgIcon icon="user" />
-          </span>
 
-          <el-input
-            name="username"
-            type="text"
-            :placeholder="$t('msg.login.userPlaceholder')"
-            v-model="formData.username"
-          ></el-input>
-        </el-form-item>
-
-        <el-form-item prop="password">
-          <span class="svg">
-            <SvgIcon icon="password" />
-          </span>
-
-          <el-input
-            class="pwd"
-            name="password"
-            :type="passwordType"
-            :placeholder="$t('msg.login.pwdPlaceholder')"
-            v-model="formData.password"
-          ></el-input>
-
-          <span class="show-pwd" @click="isShowPwd = !isShowPwd">
-            <SvgIcon v-if="!isShowPwd" icon="eye" />
-            <SvgIcon v-else icon="eye-open" />
-          </span>
-        </el-form-item>
-
-        <el-button
-          type="primary"
-          style="width: 100%; margin-top: 10px; margin-bottom: 30px;"
-          :loading="loading"
-          @click="handleLoginClick"
-        >{{ $t('msg.login.loginBtn') }}</el-button>
-
-        <div class="tips" v-html="$t('msg.login.desc')"></div>
-      </el-form>
-    </div>
+      <div class="tips" v-html="$t('msg.login.desc')"></div>
+    </el-form>
   </div>
 </template>
 
@@ -124,81 +121,96 @@ $dark_gray: #889aa4;
 $light_gray: #eee;
 $cursor: #fff;
 
-.login {
-  position: relative;
-  overflow: hidden;
+.login-container {
+  min-height: 100%;
   width: 100%;
-  height: 100%;
   background-color: $bg;
+  overflow: hidden;
 
   .login-form {
-    position: absolute;
-    top: 50%;
-    left: 50%;
+    position: relative;
     width: 520px;
-    transform: translate(-50%, -50%);
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
 
-    .form-title {
-      position: relative;
-      margin-bottom: 40px;
-      text-align: center;
-      font-size: 40px;
-      color: #fff;
-      font-weight: bold;
-      :deep(.lang-select) {
-        position: absolute;
-        top: 19px;
-        right: 0;
-        padding: 4px;
-        font-size: 22px;
-        border-radius: 4px;
-        background-color: #fff;
-        cursor: pointer;
-      }
-    }
-    
-    ::v-deep .el-form-item__content {
-      position: relative;
-      display: flex;
-      align-items: center;
-      height: 45px;
-      color: #454545;
+    ::v-deep .el-form-item {
       border: 1px solid rgba(255, 255, 255, 0.1);
-      border-radius: 5px;
       background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
 
-      .svg {
-        padding: 6px 15px 6px 15px;
-        color: $dark_gray;
-      }
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
 
       input {
-        height: 45px;
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
         color: $light_gray;
-        border: 0;
-        border-radius: 0;
-        background-color: transparent;
+        height: 47px;
         caret-color: $cursor;
       }
+    }
+  }
 
-      .pwd {
-        padding-right: 13px;
-      }
+  .tips {
+    font-size: 16px;
+    line-height: 28px;
+    color: #fff;
+    margin-bottom: 10px;
 
-      .show-pwd {
-        position: absolute;
-        right: 10px;
-        top: 5px;
-        font-size: 16px;
-        color: $dark_gray;
-        cursor: pointer;
-        user-select: none;
+    span {
+      &:first-of-type {
+        margin-right: 16px;
       }
     }
+  }
 
-    .tips {
-      color: #fff;
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
     }
+
+    ::v-deep .lang-select {
+      position: absolute;
+      top: 4px;
+      right: 0;
+      background-color: white;
+      font-size: 22px;
+      padding: 4px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
   }
 }
 </style>
