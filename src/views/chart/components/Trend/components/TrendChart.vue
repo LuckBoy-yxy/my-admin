195a0588cb89d3f2647de0e2cab079ac8dd68b37
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-13 15:32:10 +0800

    趋势图表组件, 柱状图构建逻辑已完成

--- src/views/chart/components/Trend/components/TrendChart.vue
+++ src/views/chart/components/Trend/components/TrendChart.vue
@@ -1,11 +1,45 @@
 <script setup>
+  import { ref, onMounted } from 'vue'
+  import * as echars from 'echarts'
 
+  let myChart
+  const target = ref(null)
+  onMounted(() => {
+    myChart = echars.init(target.value)
+    renderChart()
+  })
+
+  const renderChart = () => {
+    const options = {
+      title: {
+        text: 'Echars 入门示例'
+      },
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+
+    myChart.setOption(options)
+  }
 </script>
 
 <template>
-  <div>趋势图表模块</div>
+  <div class="trend-chart-container" ref="target"></div>
 </template>
 
 <style lang="scss" scoped>
-
+.trend-chart-container {
+  height: 100%;
+}
 </style>
