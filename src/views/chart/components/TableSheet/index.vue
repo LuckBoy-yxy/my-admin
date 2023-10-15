e803e2170ade5d81099f3b7852d620f7c6dfc292
yuanxiaoran <hmxs_hmbb@163.com>
2023-10-15 12:27:14 +0800

    大区营销数据分析组件基础渲染已完成

--- src/views/chart/components/TableSheet/index.vue
+++ src/views/chart/components/TableSheet/index.vue
@@ -1,14 +1,21 @@
 <script setup>
-
+  import S2 from './components/S2.vue'
+  import SheetLabelVue from './components/SheetLabel.vue'
 </script>
 
 <template>
-  <div class="container">S2 表格数据图示</div>
+  <el-row :gutter="20">
+    <el-col :span="18">
+      <S2 />
+    </el-col>
+    <el-col :span="6">
+      <SheetLabelVue v-for="item in 6" :key="item" class="mb-20" />
+    </el-col>
+  </el-row>
 </template>
 
 <style lang="scss" scoped>
-.container {
-  height: 804px;
-  background-color: #bcbfc3;
+.mb-20 {
+  margin-bottom: 20px;
 }
 </style>
